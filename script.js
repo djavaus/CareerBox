72af5aac7308e58324683ecc806ab20e21d31889
Rustam Mavliaviev <rustam.mavliaviev@gmail.com>
2023-05-19 20:29:01 +0600

    last sections & footer

---
+++ script.js
@@ -0,0 +1,24 @@
+ 
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,         
+        dots: false,
+        // items: 5, 
+        slideTransition: `linear`,
+        autoplay: 1000,
+        smartSpeed: 1000,
+        autoplayTimeout: 1000,
+        loop: true,
+        responsive:{ 
+            0:{
+                items:2
+                
+        
+            },
+
+            431:{
+                items:5
+            }
+        }
+    });
+});
